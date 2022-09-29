cdc27fe81a573922233807d0c5ea5b9c2880d0e2
mirajmalado <diallomalado8@gmail.com>
2022-09-29 13:18:00 +0000

    Update la partie Categorie

--- resources/views/admins/categories/index.blade.php
+++ resources/views/admins/categories/index.blade.php
@@ -60,7 +60,7 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                   </div>
                                   <div class="modal-body">
-                                    <p>Are you sure, you want to delete this category?</p>
+                                    <p>Etes-vous sure de vouloir supprimer cette categorie de plat?</p>
                                   </div>
                                   <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
